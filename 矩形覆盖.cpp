423cd85626299ae59bb5d88f36f744963ec23cf8
Bo-rrr <62199546+Bo-rrr@users.noreply.github.com>
2020-12-16 14:39:44 +0800

    剑指offer 10

---
+++ 矩形覆盖.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int rectCover(int number) {
+        if (number <= 0) {
+            return number;
+        }
+        int f1 = 1;
+        int f2 = 2;
+        int f3;
+         
+        for (int i = 3; i <= number; i++) {
+            f3 = f1 + f2;
+            f1 = f2;
+            f2 = f3;
+        }
+        return f3;
+    }
+};
