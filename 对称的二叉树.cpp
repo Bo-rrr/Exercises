46c32fd48145331a783aba3fe9cdb5b0c95a37fd
Bo-rrr <62199546+Bo-rrr@users.noreply.github.com>
2021-01-18 12:31:20 +0800

    剑指offer

---
+++ 对称的二叉树.cpp
@@ -0,0 +1,30 @@
+/*
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) :
+            val(x), left(NULL), right(NULL) {
+    }
+};
+*/
+
+class Solution {
+public:
+    bool isSymmetrical(TreeNode* pRoot)
+    {
+        if(pRoot==NULL)
+            return 1;
+        return isSym(pRoot->left, pRoot->right);
+    }
+    bool isSym(TreeNode* A,TreeNode* B)
+    {
+        if(A==NULL && B==NULL)
+            return 1;
+        if(A==NULL || B==NULL)
+            return 0;
+        if(A->val!=B->val)
+            return 0;
+        return isSym(A->left,B->right)&&isSym(A->right,B->left);
+    }
+};v
