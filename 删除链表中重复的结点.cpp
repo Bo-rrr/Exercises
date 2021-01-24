a1c9e774bb8ae83c75642281721261dc5ee47cc4
Bo-rrr <62199546+Bo-rrr@users.noreply.github.com>
2021-01-24 11:34:35 +0800

    剑指offer

---
+++ 删除链表中重复的结点.cpp
@@ -0,0 +1,35 @@
+/*
+struct ListNode {
+    int val;
+    struct ListNode *next;
+    ListNode(int x) :
+        val(x), next(NULL) {
+    }
+};
+*/
+class Solution {
+public:
+    ListNode* deleteDuplication(ListNode* pHead)
+    {
+        ListNode preHead(0);
+        preHead.next = pHead;
+        ListNode *preCurr = &preHead;
+        while(preCurr->next != nullptr){
+            if(preCurr->next->next != nullptr && preCurr->next->val == preCurr->next->next->val){
+                while(preCurr->next->next != nullptr && preCurr->next->val == preCurr->next->next->val){
+                    ListNode *temp = preCurr->next;
+                    preCurr->next = temp->next;
+                    delete temp;
+                }
+                
+                ListNode *temp = preCurr->next;
+                preCurr->next = temp->next;
+                delete temp;
+            }else{
+                preCurr = preCurr->next;
+            }
+        }
+        
+        return preHead.next;
+    }
+};
