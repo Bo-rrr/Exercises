fb8f23b1fe3977cdd8d6d60e71067c1a0c158cc8
Bo-rrr <62199546+Bo-rrr@users.noreply.github.com>
2021-01-28 15:17:08 +0800

    Add files via upload

---
+++ 寻找数组的中心索引.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sum = 0;
+        for (int num : nums) sum += num; // 求和
+        int leftSum = 0;    // 中心索引左半和
+        int rightSum = 0;   // 中心索引右半和
+        for (int i = 0; i < nums.size(); i++) {
+            leftSum += nums[i];
+            rightSum = sum - leftSum + nums[i];
+            if (leftSum == rightSum) return i;
+        }
+        return -1;
+    }
+};
