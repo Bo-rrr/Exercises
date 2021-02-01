c604968e0ccf9e58930ea0da261175e41ec64c73
Bo-rrr <62199546+Bo-rrr@users.noreply.github.com>
2021-02-01 16:55:42 +0800

    剑指offer22

--- 链表中倒数第K个结点.cpp
+++ 链表中倒数第K个结点.cpp
@@ -1,11 +1,3 @@
-/*
-struct ListNode {
-	int val;
-	struct ListNode *next;
-	ListNode(int x) :
-			val(x), next(NULL) {
-	}
-};*/
 class Solution {
 public:
     ListNode* FindKthToTail(ListNode* pListHead, unsigned int k) {
