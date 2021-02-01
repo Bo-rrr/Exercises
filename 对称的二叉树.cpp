594bdbb55e30b19c3b1f8389b54c288fadef3727
Bo-rrr <62199546+Bo-rrr@users.noreply.github.com>
2021-02-01 16:53:31 +0800

    剑指offer28

--- 对称的二叉树.cpp
+++ 对称的二叉树.cpp
@@ -1,14 +1,3 @@
-/*
-struct TreeNode {
-    int val;
-    struct TreeNode *left;
-    struct TreeNode *right;
-    TreeNode(int x) :
-            val(x), left(NULL), right(NULL) {
-    }
-};
-*/
-
 class Solution {
 public:
     bool isSymmetrical(TreeNode* pRoot)
