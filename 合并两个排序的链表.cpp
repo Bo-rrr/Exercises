4ac1a215d9a8da68bb09dcbff2a534beeb912e61
Bo-rrr <62199546+Bo-rrr@users.noreply.github.com>
2021-02-01 16:51:25 +0800

    剑指offer25

--- 合并两个排序的链表.cpp
+++ 合并两个排序的链表.cpp
@@ -1,11 +1,3 @@
-/*
-struct ListNode {
-	int val;
-	struct ListNode *next;
-	ListNode(int x) :
-			val(x), next(NULL) {
-	}
-};*/
 class Solution {
 public:
     ListNode* Merge(ListNode* pHead1, ListNode* pHead2)
