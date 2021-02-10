ff6929f6ff661c3c92cc7878aff7c41c316c3d80
Bo-rrr <62199546+Bo-rrr@users.noreply.github.com>
2021-02-10 11:34:51 +0800

    leetcode 19

---
+++ 删除链表的倒数第N个结点.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode(0, head);
+        ListNode* first = head;
+        ListNode* second = dummy;
+        for (int i = 0; i < n; ++i) {
+            first = first->next;
+        }
+        while (first) {
+            first = first->next;
+            second = second->next;
+        }
+        second->next = second->next->next;
+        ListNode* ans = dummy->next;
+        delete dummy;
+        return ans;
+    }
+};
