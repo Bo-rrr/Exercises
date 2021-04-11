2624629f9a5a4bd724cc2823c317732f40c3b570
Bo-rrr <62199546+Bo-rrr@users.noreply.github.com>
2021-04-11 15:41:35 +0800

    了额头从的

---
+++ 二叉树的中序遍历.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int>& res) {
+        if (!root) {
+            return;
+        }
+        inorder(root->left, res);
+        res.push_back(root->val);
+        inorder(root->right, res);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        inorder(root, res);
+        return res;
+    }
+};
