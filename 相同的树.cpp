1a83f0c5cdb6f9113daee571fd148378d1b360e3
Bo-rrr <62199546+Bo-rrr@users.noreply.github.com>
2021-04-17 14:00:10 +0800

    leetcode 100

---
+++ 相同的树.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) {
+            return true;
+        } else if (p == nullptr || q == nullptr) {
+            return false;
+        } else if (p->val != q->val) {
+            return false;
+        } else {
+            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        }
+    }
+};
