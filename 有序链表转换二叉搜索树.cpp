24e2059a5a15584077060f803da2244440425220
Bo-rrr <62199546+Bo-rrr@users.noreply.github.com>
2021-04-26 15:18:39 +0800

    leetcode 109

---
+++ 有序链表转换二叉搜索树.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int getLength(ListNode* head) {
+        int ret = 0;
+        for (; head != nullptr; ++ret, head = head->next);
+        return ret;
+    }
+
+    TreeNode* buildTree(ListNode*& head, int left, int right) {
+        if (left > right) {
+            return nullptr;
+        }
+        int mid = (left + right + 1) / 2;
+        TreeNode* root = new TreeNode();
+        root->left = buildTree(head, left, mid - 1);
+        root->val = head->val;
+        head = head->next;
+        root->right = buildTree(head, mid + 1, right);
+        return root;
+    }
+
+    TreeNode* sortedListToBST(ListNode* head) {
+        int length = getLength(head);
+        return buildTree(head, 0, length - 1);
+    }
+};
